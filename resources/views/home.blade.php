579feffcb2073e1f9fdc9b82e16a2eb33fb4980a
WeiAnAn <j22461615@gmail.com>
2017-04-27 18:20:02 +0800

    add booking table

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,46 @@
 
 @section('content')
     <div class="row">
-        <div id="datepicker"></div>
-        <input type="hidden" id="my_hidden_input">
+        <div class="col-md-3">
+            <div id="datepicker"></div>
+            <input type="hidden" id="my_hidden_input">
+        </div>
+        <div class="col-md-9">
+            <div id="booking-table">
+                <table class="table table-striped table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>教室</th>
+                            <th>時間</th>
+                            <th>課程名稱</th>
+                            <th>借用人</th>
+                            <th>時數</th>
+                        </tr>
+                    </thead>
+                    <tr>
+                        <td>123</td>
+                        <td>123</td>
+                        <td>123</td>
+                        <td>123</td>
+                        <td>123</td>
+                    </tr>
+                    <tr>
+                        <td>123</td>
+                        <td>123</td>
+                        <td>123</td>
+                        <td>123</td>
+                        <td>123</td>
+                    </tr>
+                    <tr>
+                        <td>123</td>
+                        <td>123</td>
+                        <td>123</td>
+                        <td>321</td>
+                        <td>123</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
     </div>
 
 
