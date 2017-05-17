7ec992482935fbfcb49dcb809a61b58578b0c5f7
ccumis <ccumis@ubuntu>
2017-05-18 00:18:32 +0800

    update for classroom reserve

--- resources/views/user/room_reserve.blade.php
+++ resources/views/user/room_reserve.blade.php
@@ -19,8 +19,8 @@
                 <select class="form-control">
                     <option>上課</option>
                     <option>考試</option>
-                    <option>meeting</option>
-                    <option>辦活動</option>
+                    <option>會議</option>
+                    <option>活動</option>
                 </select>
             
             </div>    
@@ -38,7 +38,97 @@
                         <span class="glyphicon glyphicon-calendar"></span>
                     </span>
                 </div>
+                
+                <label class="usrlabel">時間</label>
+                <div class="controls form-inline">
+                    <div class="input-append">
+                        <select class="usrselect">
+                            <option value="08">08</option>
+                            <option value="09">09</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                            <option value="13">13</option>
+                            <option value="14">14</option>
+                            <option value="15">15</option>
+                            <option value="16">16</option>
+                            <option value="17">17</option>
+                            <option value="18">18</option>
+                            <option value="19">19</option>
+                            <option value="20">20</option>
+                            <option value="21">21</option>
+                            <option value="22">22</option>
+                        </select>
+                        <span class="usradd-on">點</span>
+                    </div>
+                    <div class="input-append">
+                        <select class="usrselect">
+                            <option value="00">00</option>
+                            <option value="05">05</option>
+                            <option value="10">10</option>
+                            <option value="15">15</option>
+                            <option value="20">20</option>
+                            <option value="25">25</option>
+                            <option value="30">30</option>
+                            <option value="35">35</option>
+                            <option value="40">40</option>
+                            <option value="45">45</option>
+                            <option value="50">50</option>
+                            <option value="55">55</option>
+                        </select>
+                        <span class="usradd-on">分</span>
+                    </div>
+                    <span class="usrspan">~</span>    
+                    <div class="input-append">
+                        <select class="usrselect">
+                            <option value="08">08</option>
+                            <option value="09">09</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                            <option value="13">13</option>
+                            <option value="14">14</option>
+                            <option value="15">15</option>
+                            <option value="16">16</option>
+                            <option value="17">17</option>
+                            <option value="18">18</option>
+                            <option value="19">19</option>
+                            <option value="20">20</option>
+                            <option value="21">21</option>
+                            <option value="22">22</option>
+                        </select>
+                        <span class="usradd-on">點</span>
+                    </div>
+                    <div class="input-append">
+                        <select class="usrselect">
+                            <option value="00">00</option>
+                            <option value="05">05</option>
+                            <option value="10">10</option>
+                            <option value="15">15</option>
+                            <option value="20">20</option>
+                            <option value="25">25</option>
+                            <option value="30">30</option>
+                            <option value="35">35</option>
+                            <option value="40">40</option>
+                            <option value="45">45</option>
+                            <option value="50">50</option>
+                            <option value="55">55</option>
+                        </select>
+                        <span class="usradd-on">分</span>
+                    </div>
+                </div>
+            
             </div>
+            <div class="col-xs-3 usrclassinfo">
+                <label class="usrlabel">教室</label>
+                <select class="form-control">
+                    <option>216</option>
+                    <option>349</option>
+                    <option>512</option>
+                    <option>617</option>
+                </select>
+            </div>
+                
         </div>    
     </div>
 </div>
