2aa1d8f9e37127ca8df6abf82fe30e6ea24698ab
Wei-An <j22461615@gmail.com>
2017-05-26 23:49:41 +0800

    update room_reserve function

--- app/Http/Controllers/ClassroomRecordController.php
+++ app/Http/Controllers/ClassroomRecordController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\ClassroomRecord;
+use App\Classroom;
+use Auth;
 
 class ClassroomRecordController extends Controller
 {
@@ -12,5 +14,44 @@ class ClassroomRecordController extends Controller
             ->where('date',$request->date)
             ->get();
         return $data->toJson();
+    }
+
+    function roomReserveIndex(){
+        $classrooms = Classroom::all();
+        $arr = compact('classrooms');
+        return view('user.room_reserve',$arr);
+    }
+
+    function addRoomReserve(Request $request){
+        $user = Auth::user();
+        $arr = $request->all();
+        $arr['status'] = 0;
+        $arr['borrower'] = $user->name;
+        $arr['user_id'] = $user->id;
+        $arr['start_time'] = $request->startHour.":".$request->startMin;
+        $arr['end_time'] = $request->endHour.":".$request->endMin;
+        ClassroomRecord::create($arr);
+        return redirect('/user/room_reserve');
+    }
+
+    function showRoomReserve(){
+        $records = ClassroomRecord::with('classroom')
+            ->with('user')
+            ->where('status', 0)
+            ->get();
+        $data = compact('records');
+        return view('/admin/room_reserve_manage', $data);
+    }
+
+    function acceptRoomReserve($id){
+        ClassroomRecord::where('id',$id)
+            ->update(['status'=>1]);
+            
+        return redirect('/admin/room_reserve_manage');
+    }
+    function rejectRoomReserve($id,Request $request){
+        ClassroomRecord::where('id',$id)
+            ->update(['status'=>-1,'reason'=>$request->reason]);
+        return redirect('/admin/room_reserve_manage');
     }
 }
--- resources/views/user/room_reserve.blade.php
+++ resources/views/user/room_reserve.blade.php
@@ -7,21 +7,22 @@
     </div> 
     <!-- Tab panes -->
         <div class="tab-content col-md-8">
-            <form action="#" method="post">
+            <form action="{{URL('/user/room_reserve_add')}}" method="post">
+                {{csrf_field()}}
                 <div class="usrcontent">填寫課程資訊</div>
                 <div class="custom_seperator"></div>
                 
                 <div class="row">
                     <div class="col-xs-4 usrclassinfo">
                         <label class="usrlabel">課程名稱</label>
-                        <input type="text" class="form-control">
+                        <input type="text" class="form-control" name="name">
                         
                         <label class="usrlabel">事由</label>
-                        <select class="form-control">
-                            <option>上課</option>
-                            <option>考試</option>
-                            <option>會議</option>
-                            <option>活動</option>
+                        <select class="form-control" name="type">
+                            <option value="1">上課</option>
+                            <option value="2">考試</option>
+                            <option value="3">會議</option>
+                            <option value="4">活動</option>
                         </select>
                     
                     </div>    
@@ -34,7 +35,7 @@
                     <div class="col-xs-4 usrclassinfo">
                         <label class="usrlabel">日期</label>
                         <div class='input-group' >
-                            <input type="text" class="form-control datepicker" id="datepicker1">
+                            <input type="text" class="form-control datepicker" id="datepicker1" name="date" >
                             <span class="input-group-addon">
                                 <span class="glyphicon glyphicon-calendar"></span>
                             </span>
@@ -43,7 +44,7 @@
                         <label class="usrlabel">時間</label>
                         <div class="controls form-inline">
                             <div class="input-append">
-                                <select class="usrselect">
+                                <select class="usrselect" name="startHour">
                                     <option value="08">08</option>
                                     <option value="09">09</option>
                                     <option value="10">10</option>
@@ -63,7 +64,7 @@
                                 <span class="usradd-on">點</span>
                             </div>
                             <div class="input-append">
-                                <select class="usrselect">
+                                <select class="usrselect" name="startMin">
                                     <option value="00">00</option>
                                     <option value="05">05</option>
                                     <option value="10">10</option>
@@ -81,7 +82,7 @@
                             </div>
                             <span class="usrspan">~</span>    
                             <div class="input-append">
-                                <select class="usrselect">
+                                <select class="usrselect" name="endHour">
                                     <option value="08">08</option>
                                     <option value="09">09</option>
                                     <option value="10">10</option>
@@ -101,7 +102,7 @@
                                 <span class="usradd-on">點</span>
                             </div>
                             <div class="input-append">
-                                <select class="usrselect">
+                                <select class="usrselect" name="endMin">
                                     <option value="00">00</option>
                                     <option value="05">05</option>
                                     <option value="10">10</option>
@@ -122,11 +123,10 @@
                     </div>
                     <div class="col-xs-3 usrclassinfo">
                         <label class="usrlabel">教室</label>
-                        <select class="form-control">
-                            <option>216</option>
-                            <option>349</option>
-                            <option>512</option>
-                            <option>617</option>
+                        <select class="form-control" name="classroom_id">
+                            @foreach($classrooms as $classroom)
+                            <option value="{{$classroom->id}}">{{$classroom->name." (人數:$classroom->count)"}}</option>
+                            @endforeach
                         </select>
                     </div>
                 </div><!--row of time and classroom-->    
@@ -230,7 +230,11 @@
 </div>
 
 <script type="text/javascript">
-$('#datepicker1').datepicker();
+$('#datepicker1').datepicker({
+    format: "yyyy-mm-dd",
+    language: "zh-TW",
+    autoclose: true
+});
 </script>
 
 @endsection    
