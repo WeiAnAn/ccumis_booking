d15383042c137c6ed2e0b5b65d9266d65f3a9ddd
Wei-An <j22461615@gmail.com>
2017-05-30 02:14:16 +0800

    add relation

--- app/ClassroomRecord.php
+++ app/ClassroomRecord.php
@@ -7,14 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class ClassroomRecord extends Model
 {
     protected $fillable = ['classroom_id', 'date', 'start_time',
-        'end_time', 'return_datetime', 'status', 'user_id', 
-        'borrower', 'semester_class_id', 'name', 'borrow_time',
-        'type', 'reason'];
+        'end_time', 'return_datetime', 'status', 'borrow_user_id', 
+        'borrower', 'semester_class_id', 'name', 'borrow_datetime',
+        'type', 'reason', 'reserve_user_id'];
     
     public function classroom(){
         return $this->hasOne('App\Classroom', 'id', 'classroom_id');
     }
-    public function user(){
-        return $this->hasOne('App\User', 'id', 'user_id');
+    public function reserver(){
+        return $this->hasOne('App\User', 'id', 'reserve_user_id');
+    }
+    public function borrower(){
+        return $this->hasOne('App\User', 'id', 'borrow_user_id');
     }
 }
