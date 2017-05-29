396e511bd491d30679afbdca429f45221658edc5
Wei-An <j22461615@gmail.com>
2017-05-30 02:17:27 +0800

    update user navigation

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -1,10 +1,16 @@
 <!-- Nav -->
 <ul class="nav nav-pills nav-stacked custom_adposition">
-    <li role="presentation" class="custom_seperator" >
-        <a href="{{URL('/user/room_reserve')}}">預約教室</a>
+    <li class="custom_seperator">
+        <a href="{{URL('/user/not_returned')}}">尚未歸還教室與設備</a>
+    </li>
+    <li class="custom_seperator">
+        <a href="{{URL('/user/room_borrow')}}">教室借用</a>
     </li>
     <li role="presentation" class="custom_seperator">
         <a href="{{URL('/user/equipment_borrow')}}">設備借用</a>
+    </li>
+    <li role="presentation" class="custom_seperator" >
+        <a href="{{URL('/user/room_reserve')}}">預約教室</a>
     </li>
     <li role="presentation" class="custom_seperator">
         <a href="{{URL('/user/review_reserve')}}">審核中預約</a>
