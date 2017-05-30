5c15df69b5d1bec9d35ba35e1f8ecb42b45c5e23
Wei-An <j22461615@gmail.com>
2017-05-30 16:58:46 +0800

    update admin navigation

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,8 +1,5 @@
 <!-- Nav -->
 <ul class="nav nav-pills nav-stacked custom_adposition">
-    <li role="presentation" class="custom_seperator" >
-        <a href="{{URL('/admin/room_record')}}">教室借閱紀錄</a>
-    </li>
     <li class="custom_seperator">
         <a href="{{URL('/admin/room_reserve_manage')}}">待審核預約</a>
     </li>
@@ -18,8 +15,14 @@
     <li class="custom_seperator">
         <a href="{{URL('/admin/semester_manage')}}">學期資料管理</a>
     </li>
-    <li>
+    <li class="custom_seperator">
         <a href="{{URL('/admin/semester_class_manage')}}">學期課程管理</a>
+    </li>
+    <li class="custom_seperator">
+        <a href="{{URL('/admin/classroom_history')}}">教室借用歷史紀錄</a>
+    </li>
+    <li class="custom_seperator">
+        <a href="{{URL('/admin/equipment_history')}}">設備借用歷史紀錄</a>
     </li>
 
 </ul>
