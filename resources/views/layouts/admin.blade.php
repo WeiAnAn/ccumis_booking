9a0a413f6cca5d79fe228a09023b3b60def3cb85
Wei-An <j22461615@gmail.com>
2017-05-31 01:16:14 +0800

    improve rwd

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,5 +1,5 @@
 <!-- Nav -->
-<ul class="nav nav-pills nav-stacked custom_adposition">
+<ul class="nav nav-pills nav-stacked custom_adposition visible-lg visible-md">
     <li class="custom_seperator">
         <a href="{{URL('/admin/not_returned')}}">未歸還設備教室</a>
     </li>
@@ -27,12 +27,55 @@
     <li class="custom_seperator">
         <a href="{{URL('/admin/equipment_history')}}">設備借用歷史紀錄</a>
     </li>
-
 </ul>
+
+<div class="panel panel-default hidden-lg hidden-md">
+    <div class="panel-heading" role="tab" id="tab" onclick="collapse()">
+        <h4 class="panel-title">
+            <a href="#collapse" role="button" data-toggle="collapse" 
+                aria-expanded="false" aria-controls="collapse">功能選單</a>
+        </h4>
+    </div>
+    <div class="panel-collapse collapse" role="tabpanel" id="collapse" 
+        aria-labelledby="tab" aria-expanded="false">
+        <ul class="list-group">
+            <li class="list-group-item">
+                <a href="{{URL('/admin/not_returned')}}">未歸還設備教室</a>
+            </li>
+            <li class="list-group-item">
+                <a href="{{URL('/admin/room_reserve_manage')}}">待審核預約</a>
+            </li>
+            <li class="list-group-item">
+                <a href="{{URL('/admin/room_reserve_completed')}}">已審核預約</a>
+            </li>
+            <li role="presentation" class="list-group-item">
+                <a href="{{URL('/admin/room_manage')}}">教室管理</a>
+            </li>
+            <li role="presentation" class="list-group-item">
+                <a href="{{URL('/admin/equipment_manage')}}">設備管理</a>
+            </li>
+            <li class="list-group-item">
+                <a href="{{URL('/admin/semester_manage')}}">學期資料管理</a>
+            </li>
+            <li class="list-group-item">
+                <a href="{{URL('/admin/semester_class_manage')}}">學期課程管理</a>
+            </li>
+            <li class="list-group-item">
+                <a href="{{URL('/admin/classroom_history')}}">教室借用歷史紀錄</a>
+            </li>
+            <li class="list-group-item">
+                <a href="{{URL('/admin/equipment_history')}}">設備借用歷史紀錄</a>
+            </li>
+        </ul>
+    </div>
+</div>
 
 <script>
 $('.nav-pills li').click( function() {
       $('.navpills li').removeClass('active');
       $(this).addClass('active');
 });
+function collapse(){
+    $('#collapse').collapse('toggle');
+}
 </script>
--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -1,5 +1,5 @@
 <!-- Nav -->
-<ul class="nav nav-pills nav-stacked custom_adposition">
+<ul class="nav nav-pills nav-stacked custom_adposition visible-lg visible-md">
     <li class="custom_seperator">
         <a href="{{URL('/user/not_returned')}}">歸還教室與設備</a>
     </li>
@@ -23,9 +23,48 @@
     </li>
 </ul>
 
+<div class="panel panel-default hidden-lg hidden-md">
+    <div class="panel-heading" role="tab" id="tab" onclick="collapse()">
+        <h4 class="panel-title">
+            <a href="#collapse" role="button" data-toggle="collapse" 
+                aria-expanded="false" aria-controls="collapse">功能選單</a>
+        </h4>
+    </div>
+    <div class="panel-collapse collapse" role="tabpanel" id="collapse" 
+        aria-labelledby="tab" aria-expanded="false">
+        <ul class="list-group">
+            <li class="list-group-item">
+                <a href="{{URL('/user/not_returned')}}">歸還教室與設備</a>
+            </li>
+            <li class="list-group-item">
+                <a href="{{URL('/user/room_borrow')}}">教室借用</a>
+            </li>
+            <li role="presentation" class="list-group-item">
+                <a href="{{URL('/user/equipment_borrow')}}">設備借用</a>
+            </li>
+            <li role="presentation" class="list-group-item" >
+                <a href="{{URL('/user/room_reserve')}}">預約教室</a>
+            </li>
+            <li role="presentation" class="list-group-item">
+                <a href="{{URL('/user/review_reserve')}}">審核中預約</a>
+            </li>
+            <li class="list-group-item">
+                <a href="{{URL('/user/classroom_history')}}">教室借用歷史紀錄</a>
+            </li>
+            <li class="list-group-item">
+                <a href="{{URL('/user/equipment_history')}}">設備借用歷史紀錄</a>
+            </li>
+        </ul>
+    </div>
+</div>
+
 <script>
 $('.nav-pills li').click( function() {
       $('.nav-pills li').removeClass('active');
       $(this).addClass('active');
 });
+
+function collapse(){
+    $('#collapse').collapse('toggle');
+}
 </script>
