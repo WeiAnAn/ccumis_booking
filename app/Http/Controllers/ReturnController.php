01f16787ccb784e2599778bb2d324f9f5bae4e96
Wei-An <j22461615@gmail.com>
2017-05-30 16:59:05 +0800

    finish admin return function

--- app/Http/Controllers/ReturnController.php
+++ app/Http/Controllers/ReturnController.php
@@ -42,6 +42,41 @@ class ReturnController extends Controller
                 $record->return_datetime = date("Y-m-d H:i:s");
                 $record->save();
             }
-        dump($equipment, $classrooms);
+        return redirect('/user/not_returned');
+    }
+
+    public function adminIndex(){
+        $classrooms = ClassroomRecord::with('classroom')
+            ->with('user')
+            ->whereNotNull('borrow_datetime')
+            ->whereNull('return_datetime')
+            ->get();
+        $equipment = EquipmentRecord::with('equipment')
+            ->with('user')
+            ->whereNotNull('borrow_datetime')
+            ->whereNull('return_datetime')
+            ->get();
+        $data = compact('classrooms', 'equipment');
+        return view('admin.not_returned', $data);
+    }
+
+    public function adminReturn(Request $request){
+        $equipment = $request->item;
+        $classrooms = $request->classroom;
+        if(gettype($classrooms) == "array")
+            foreach($classrooms as $classroom){
+                ClassroomRecord::where('id', $classroom)
+                    ->update(['return_datetime'=> date("Y-m-d H:i:s")]);
+            }
+        if(gettype($equipment) == "array")
+            foreach($equipment as $id){
+                $record = EquipmentRecord::find($id);
+                $item = Equipment::find($record->equipment_id);
+                $item->remain = $item->remain + $record->count;
+                $item->save();
+                $record->return_datetime = date("Y-m-d H:i:s");
+                $record->save();
+            }
+        return redirect('/admin/not_returned');
     }
 }
